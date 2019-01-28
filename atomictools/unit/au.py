b5c560df8061687e77075eb25a084f18797a3a70
akhdhys <akihide.h@icloud.com>
2019-01-29 00:03:40 +0900

    Added unit.si and modified unit.au

--- atomictools/unit/au.py
+++ atomictools/unit/au.py
@@ -1,20 +1,20 @@
 from math import pi
 
-hartree = 1.0                         # energy
-me = 1.0                              # mass
-bohr = 1.0                            # length
-e = 1.0                               # charge
-hbar = 1.0                            # action
+Eh = 1.0                               # energy (hartee)
+me = 1.0                               # mass of electron
+a0 = 1.0                               # length (bohr radius)
+e  = 1.0                               # charge of electron
+hbar = 1.0                             # action (h / 2pi)
 
-alpha = 7.2973525664E-3               # fine-structure constant
-alpha_inv = 137.035999139             # 1 / alpha
-c = alpha_inv                         # speed of light
+alpha = 7.2973525664E-3                # fine-structure constant
+alpha_inv = 137.035999139              # 1 / alpha
+c = alpha_inv                          # speed of light
 
-J = 2.2937123163853187e+17 * hartree  # joule
-kg = 1.0977691228098864e+30 * me      # kilogram
-m = 1.8897261255E+10 * bohr           # metre
-C = 6.241510734005358e+18 * e         # coulomb
-Js = 9.48252172113838e+33 * hbar      # joule second
+J = 2.2937123163853187e+17 * Eh        # joule
+kg = 1.0977691228098864e+30 * me       # kilogram
+m = 1.8897261255E+10 * a0              # metre
+C = 6.241510734005358e+18 * e          # coulomb
+Js = 9.48252172113838e+33 * hbar       # joule second
 
 s = Js / J
 m_s = m / s
@@ -24,11 +24,11 @@ V_m = V / m
 eV = e * V
 Hz = 1 / s
 THz = 1E12 * Hz
-k = 1.380649E-23 * J # / K                # Boltzmann constant have to be defined with Kelvin. Kelvin can be converted to energy by multiply k
+kb = 1.380649E-23 * J # / K            # Boltzmann constant have to be defined with Kelvin. Kelvin can be converted to energy by multiply k
 Pa = kg / (m * s * s)
 atm = 101325 * Pa
 
-dalton = 1822.8884853323707
+Da = 1822.8884853323707
 angstrom = m * 1E-10
 
 mol = 6.022140857E23
@@ -37,5 +37,3 @@ cal = 4.184 * J
 
 kayser = 100 / m * hbar * 2 * pi * c
 
-# cm_1 = 0.725163330219952E-06
-# cm_1 = 0.0000045563352812122295
---
+++ atomictools/unit/si.py
@@ -0,0 +1,31 @@
+from math import pi
+
+m = 1.0
+s = 1.0
+kg = 1.0
+A = 1.0
+mol = 1.0
+cd = 1.0
+K = 1.0
+
+C = s * A
+N = m * kg / (s * s)
+Pa = N / (m * m)
+J = N * m
+W = J / s
+
+
+h  = 6.62607015E-34 * J * s                    # Planck constant
+e  = 1.602176634E-19 * C                       # elementary charge
+kb = 1.380649E-23 * J / K                      # Boltzmann constant
+NA = 6.02214076E23 / mol                      # Avogadro constant
+c  = 299792458 * m / s                         # speed of light
+
+hbar = h / (2 * pi)
+me = 9.10938356E-31 * kg
+a0 = 0.52917721067E-10 * m # Bohr radius
+Eh = 4.359744650E-18 * J   # hartree
+
+Da = 1.660539040E-27 * kg
+cal = 4.184 * J
+eV = 1.6021766208E-19 * J
